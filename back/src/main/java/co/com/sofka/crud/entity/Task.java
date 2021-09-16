00a0e26b7e22dba95b68b1257285ed215142c26b
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-16 11:30:54 -0500

    Clases de entidad terminadas

--- back/src/main/java/co/com/sofka/crud/entity/Task.java
+++ back/src/main/java/co/com/sofka/crud/entity/Task.java
@@ -1,10 +1,8 @@
 package co.com.sofka.crud.entity;
 
-import javax.persistence.*;
-
 import lombok.*;
 
-import java.util.List;
+import javax.persistence.*;
 
 @Data
 @Getter
@@ -12,17 +10,20 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="todo")
-public class ToDo {
+@Table(name="task")
+public class Task {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
 
     @Column(name="name")
     private String name;
 
-    @OneToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
-    @JoinColumn(name="course_id")
-    private List<Task> tasks;
+    @Column(name="completed")
+    private boolean completed;
+
+    @Column(name="id_todo")
+    private Long idTodo;
 }
