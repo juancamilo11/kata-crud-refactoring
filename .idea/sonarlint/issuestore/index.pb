b68f967f7c2d5c453581c67a770046c08030002d
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-16 22:24:08 -0500

    Fetch de listas de tareas terminado

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+back/pom.xml,e/a/ea3d82eba0d3ead41a6cf41b81f6ffc13f326c3e
+i
+9back/src/main/java/co/com/sofka/crud/CrudApplication.java,6/f/6f7492e2213636527c536fc5eceaebe6b751a807
--- front/src/App.js
+++ front/src/App.js
@@ -125,30 +125,38 @@ const List = () => {
   const decorationDone = {
     textDecoration: 'line-through'
   };
+
   return <div>
-    <table >
-      <thead>
-        <tr>
-          <td>ID</td>
-          <td>Tarea</td>
-          <td>¿Completado?</td>
-        </tr>
-      </thead>
-      <tbody>
-        {currentList.map((todo) => {
-          return <tr key={todo.id} style={todo.completed ? decorationDone : {}}>
-            <td>{todo.id}</td>
-            <td>{todo.name}</td>
-            <td><input type="checkbox" defaultChecked={todo.completed} onChange={(event) => onChange(event, todo)}></input></td>
-            <td><button onClick={() => onDelete(todo.id)}>Eliminar</button></td>
-            <td><button onClick={() => onEdit(todo)}>Editar</button></td>
-          </tr>
-        })}
-      </tbody>
-    </table>
+
+    {currentList.map((todo) => { 
+      
+      return <div>
+      <h2>{todo.name}</h2> 
+      <table key={todo.id}>
+          
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>Descripción</td>
+              <td>¿Completado?</td>
+            </tr>
+          </thead>
+          <tbody>
+            {todo.tasks.map((task) => { 
+              return <tr key={task.id}>
+                <td>{task.id}</td>
+                <td>{task.name}</td>
+                <td><input type="checkbox" defaultChecked={task.completed} onChange={(event) => onChange(event, todo)}></input></td>
+                <td><button onClick={() => onDelete(task.id)}>Eliminar</button></td>
+                <td><button onClick={() => onEdit(task)}>Editar</button></td>
+              </tr>
+            })}
+          </tbody>
+        </table>
+      </div>
+    })}
   </div>
 }
-
 
 
 function reducer(state, action) {
